ec3dae4e804fe202813eba0e7704f10abe70e1b3
Data-123 <Data-123@host.docker.internal>
2022-06-22 18:29:10 +0900

    set response status as created

--- src/main/java/com/kyobong/store/controller/TokenController.java
+++ src/main/java/com/kyobong/store/controller/TokenController.java
@@ -1,5 +1,6 @@
 package com.kyobong.store.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kyobong.store.security.JwtTokenUtil;
@@ -26,8 +28,9 @@ public class TokenController {
 	
 	private final JwtUserDetailsService userDetailsService;
 	
+	@ResponseStatus(code = HttpStatus.CREATED)
 	@RequestMapping(value = "/token", method = RequestMethod.POST)
-	public ResponseEntity<JwtResponse> createToken(@RequestBody JwtRequest request) {
+	public JwtResponse createToken(@RequestBody JwtRequest request) {
 		String username = request.getUsername();
 		String password = request.getPassword();
 		
@@ -36,7 +39,7 @@ public class TokenController {
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 		
 		final String token = tokenUtil.generateToken(userDetails);
-		return ResponseEntity.ok(new JwtResponse(token));
+		return new JwtResponse(token);
 		
 	}
 
